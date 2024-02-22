ca23cba621352e8ccffd8eed045f6158477a10d3
nbzw <2958556459@qq.com>
2024-02-22 22:54:42 +0800

    更新代码

---
+++ ui/src/components/Index.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="layout">
+    <Row class="flex" type="flex">
+      <Col span="5" class="layout-menu-left">
+      <Menu active-name="1-1" theme="dark" width="auto" :open-names="['1']">
+        <div class="layout-logo-left">
+          <h2 style="color:white;">图书馆</h2>
+        </div>
+        <Submenu name="1">
+          <template slot="title">
+            欢迎你，超級管理员
+          </template>
+          <MenuItem name="1-1" @click.native="lookUser"><span>用户管理</span></MenuItem>
+        </Submenu>
+      </Menu>
+      </Col>
+      <Col span="19">
+      <div class="layout-breadcrumb">
+        <Breadcrumb>
+          <BreadcrumbItem href="#">{{one_nav}}</BreadcrumbItem>
+          <BreadcrumbItem href="#">{{two_nav}}</BreadcrumbItem>
+          <BreadcrumbItem>{{three_nav}}</BreadcrumbItem>
+          <button id="logout" @click="logout()">退出</button>
+        </Breadcrumb>
+      </div>
+      <div class="layout-content">
+        <div class="layout-content-main">
+          <template id="userManage"></template>
+          <component :is="currentView"></component>
+        </div>
+      </div>
+      </Col>
+    </Row>
+  </div>
+</template>
+<script>
+  import userManage from '../components/UserManage.vue'
+  import Button from 'iview/src/components/button/button'
+  export default {
+    name: 'Index',
+    data () {
+      return {
+        msg: 'haha',
+        one_nav: '主页',
+        two_nav: '后台管理',
+        three_nav: '用户管理',
+        currentView: 'userManage'
+      }
+    },
+    methods: {
+      lookUser () {
+        this.currentView = 'userManage'
+        this.one_nav = '主页'
+        this.two_nav = '后台管理'
+        this.three_nav = '用户管理'
+        this.currentView = 'userManage'
+      },
+      logout () {
+        this.$router.replace('/')
+      }
+    },
+    components: {
+      Button,
+      userManage: userManage
+    }
+  }
+</script>
+<style scoped>
+  .layout{
+    position: relative;
+    margin-top:-60px;
+    height: 100vh;
+    widows: 100wh;
+  }
+  .flex {
+    height: 100vh;
+    widows: 100wh;
+  }
+  .layout-breadcrumb{
+    padding: 10px 15px 0;
+    text-align:left;
+  }
+  .layout-content{
+    min-height: 200px;
+    margin: 15px;
+    overflow: hidden;
+    background: #fff;
+    border-radius: 4px;
+  }
+  .layout-content-main{
+    padding: 10px;
+  }
+  .layout-copy{
+    text-align: center;
+    padding: 10px 0 20px;
+    color: #9ea7b4;
+  }
+  .layout-menu-left{
+    background: #464c5b;
+  }
+  .layout-header{
+    height: 60px;
+    background: #fff;
+    box-shadow: 0 1px 1px rgba(0,0,0,.1);
+  }
+  .layout-logo-left{
+    width: 90%;
+    height: 30px;
+    margin: 15px auto;
+  }
+  Button{
+    color:white;
+  }
+  
+  /* 退出按钮 */
+  #logout{
+    position: absolute;
+    right:5%;
+    background-color:rgb(224, 74, 74);
+    width: 70px;
+    height: 30px;
+    border: #464c5b;
+    border-radius: 10%;
+  }
+</style>
---
+++ ui/src/components/Register.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="hello">
+    <Form ref="formItem" :model="formItem" :rules="ruleItem" :label-width="80">
+      <Row>
+        <Col :xs="23" :sm="23" :md="23" :lg="23">
+        <h2>{{msg}}</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col :xs="23" :sm="23" :md="23" :lg="23">
+        <FormItem prop="account" label="注册账号">
+          <Input type="text" v-model="formItem.account" placeholder="填写注册邮箱"></Input>
+        </FormItem>
+        </Col>
+      </Row>
+      <Row>
+        <Col :xs="23" :sm="23" :md="23" :lg="23">
+        <FormItem prop="name" label="姓名">
+          <Input type="text" v-model="formItem.name" placeholder="输入姓名"></Input>
+        </FormItem>
+        </Col>
+      </Row>
+      <Row>
+        <Col :xs="23" :sm="23" :md="23" :lg="23">
+        <FormItem prop="sex" label="性别">
+            <input type="radio" value="男" v-model="sex"><label>男</label>
+            <input type="radio" value="女" v-model="sex"><label>女</label>
+        </FormItem>
+        </Col>
+      </Row>
+      <Row>
+        <Col :xs="23" :sm="23" :md="23" :lg="23">
+        <FormItem prop="password" label="注册密码">
+          <Input type="password" v-model="formItem.password" placeholder="填写密码"></Input>
+        </FormItem>
+        </Col>
+      </Row>
+      <Row>
+        <Col :xs="23" :sm="23" :md="23" :lg="23">
+        <FormItem prop="conf_password" label="确认密码">
+          <Input type="password" v-model="formItem.conf_password" placeholder="重复密码"></Input>
+        </FormItem>
+        </Col>
+      </Row>
+       <!-- 注册按钮 -->
+      <br>
+      <Button id="resgiter_btn" type="primary" @click="handleSubmit('formItem')">
+          <span>注册</span>
+      </Button>
+        
+      <Row class="tip">
+        <Col :xs="10" :sm="10" :md="10" :lg="10" offset="12">
+          <span class="login_font"  @click="login()"><a>已有账号？点击登陆</a></span>
+        </Col>
+      </Row>
+    </Form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      sex: '男',
+      msg: '图书管理系统',
+      verifyCode: 'hello',
+      formItem: {
+        account: '',
+        password: '',
+        sex: '男',
+        name: ''
+      },
+      ruleItem: {
+        account: [{
+          required: true,
+          message: '请填写账号！',
+          trigger: 'blur'
+        }],
+        name: [{
+          required: true,
+          message: '请输入姓名！',
+          trigger: 'blur'
+        }],
+        password: [{
+          required: true,
+          message: '请填写密码',
+          trigger: 'blur'
+        }, {
+          type: 'string',
+          min: 3,
+          message: '密码长度不能小于6位',
+          trigger: 'blur'
+        }],
+        conf_password: [{
+          required: true,
+          message: '请填写密码',
+          trigger: 'blur'
+        }, {
+          type: 'string',
+          min: 3,
+          message: '密码长度不能小于6位',
+          trigger: 'blur'
+        }]
+      }
+    }
+  },
+  methods: {
+    handleSubmit (name) {
+      var that = this
+      this.loading = true
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          that.$http.post(that.GLOBAL.serverPath + '/user/register',
+            {
+              account: that.formItem.account,
+              sex: that.formItem.sex,
+              name: that.formItem.name,
+              password: that.formItem.password
+            },
+            {
+              emulateJSON: true
+            }
+          ).then(function (res) {
+            if (res.data.status === 'ok') {
+              this.$Message.success('注冊成功!')
+              this.$router.replace({path: '/'})
+            } else {
+              console.log("aaa")
+              this.$Message.error(res.data.message)
+              this.loading = false
+            }
+          })
+        } else {
+          this.loading = false
+        }
+      })
+    },
+    login () {
+      this.$router.replace('/')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello{
+    margin: auto;
+  }
+  h2{
+    color:#0e4683;
+    margin-bottom:20px;
+  }
+  Form{
+    margin:0 auto;
+    width: 380px;
+    border: 1px solid rgb(192, 192, 192);
+    padding:50px 0;
+    /*background-color:red;*/
+  }
+  #login_btn{
+    width:70%;
+  }
+  .tip{
+    margin-top:10px;
+    color:darkgrey;
+  }
+
+  /* 注册按钮 */
+  #resgiter_btn{
+    width:70%;
+  }
+</style>
